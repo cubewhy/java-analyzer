741ebb426ba7c5b977ecc9b0ba5fd8024bcf2d67
cubewhy <qby140326@gmail.com>
2026-03-07 14:46:07 +0800

    chore: set the default logging level to "info"

--- src/logging.rs
+++ src/logging.rs
@@ -4,8 +4,7 @@ pub(crate) fn init_logger() {
     tracing_subscriber::registry()
         .with(fmt::layer().with_writer(std::io::stderr).with_ansi(false))
         .with(
-            EnvFilter::try_from_env("JAVA_ANALYZER_LOG")
-                .unwrap_or_else(|_| EnvFilter::new("debug")),
+            EnvFilter::try_from_env("JAVA_ANALYZER_LOG").unwrap_or_else(|_| EnvFilter::new("info")),
         )
         .init();
 }
